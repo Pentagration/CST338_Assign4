e110e052d92868f2acc81f3b572ee2cdcf31344a
Adam Houser <aphouser@gmail.com>
2019-05-26 08:48:21 -0700

    Barcode printing?
    I think I have a barcode printing mostly correctly now, though not in the bottom left.

--- Assignment4.java
+++ Assignment4.java
@@ -7,128 +7,134 @@
 
 public class Assign4
 {
-   public static void main(String[] args)
-      {
-      String[] testString = new String[] 
-            {"This", "is", "a", "good", "SAMPLE", "to", "look", "at."};
-      BarcodeImage test1 = new BarcodeImage();
-      BarcodeImage test2 = new BarcodeImage(testString);
-      
-      test1.displayToConsole();
-      test2.displayToConsole();
-      }
+    public static void main(String[] args)
+    {
+        String[] testString = new String[]
+                {"This", "is", "a", "good", "SAMPLE", "to", "look", "at"};
+        //BarcodeImage test1 = new BarcodeImage();
+        BarcodeImage test2 = new BarcodeImage(testString);
+
+        //test1.displayToConsole();
+        test2.displayToConsole();
+    }
 }
 
 interface BarcodeIO
 {
-   // All Implimentations expected to store img and text
-   //BarcodeImage img;
+    // All Implimentations expected to store img and text
+    //BarcodeImage img;
 
-   //String text;
+    //String text;
 
-   public boolean scan(BarcodeImage bc);
-   public boolean readText(String text);
-   public boolean generateImageFromText();
-   public boolean translateImageToText();
-   public void displayTextToConsole();
-   public void displayImageToConsole();
+    public boolean scan(BarcodeImage bc);
+    public boolean readText(String text);
+    public boolean generateImageFromText();
+    public boolean translateImageToText();
+    public void displayTextToConsole();
+    public void displayImageToConsole();
 
 }
 
 class BarcodeImage implements Cloneable
 {
-   public static final int MAX_HEIGHT = 30;
-   public static final int MAX_WIDTH = 65;
-   //If inbound matrix is smaller than max size, instantiate to false (blank)
+    public static final int MAX_HEIGHT = 30;
+    public static final int MAX_WIDTH = 65;
+    //If inbound matrix is smaller than max size, instantiate to false (blank)
 
-   private boolean[][] imageData ;
-   // White -> false
-   // Black -> true
+    private boolean[][] imageData ;
+    // White -> false
+    // Black -> true
 
 
-   // Constructors (2 minimum)
+    // Constructors (2 minimum)
 
-   // Default Constructor
-   // Instantiates Max size array and fills with all blanks
-   public BarcodeImage()
-   {
-     this.imageData = new boolean[MAX_WIDTH][MAX_HEIGHT];
-       
-       for(int x=0;x<MAX_WIDTH;x++) 
-     {
-        for(int y=0;y<MAX_HEIGHT;y++) 
+    // Default Constructor
+    // Instantiates Max size array and fills with all blanks
+    public BarcodeImage()
+    {
+        this.imageData = new boolean[MAX_WIDTH][MAX_HEIGHT];
+
+        for(int x = 0; x < MAX_WIDTH; x++)
         {
-           this.setPixel(x, y, false);
+            for(int y = 0; y < MAX_HEIGHT; y++)
+            {
+                this.setPixel(x, y, false);
+            }
         }
-     }
-   }
+    }
 
-   // Takes 1d string array. Converts to "the internal 2d array of booleans"
-   public BarcodeImage(String[] strData)
-   {
-     this.imageData = new boolean[MAX_WIDTH][MAX_HEIGHT];
-       
-     if (checkSize(strData) == true)
-     {
-        //convert each letter to ascii then binary and set appropriately
-        //1s are *, 0s are blank
-        for (int i = 0; i < strData.length; i++) // columns
+    // Takes 1d string array. Converts to "the internal 2d array of booleans"
+    public BarcodeImage(String[] strData)
+    {
+        this.imageData = new boolean[MAX_WIDTH][MAX_HEIGHT];
+
+        if (checkSize(strData) == true)
         {
-           //pull word into character array
-           char[] chars = strData[i].toCharArray();
-           
-           for (int j = 0; j < chars.length; j++)
-           {
-              //converts letter to binary
-              int temp = Character.getNumericValue(chars[j]);
-              String temp2 = Integer.toBinaryString(temp);
-              
-              //add *s to the rows where binary = 1
-              char[] binary = temp2.toCharArray();
-              for (int k = 0; k < binary.length; k++)
-              {
-                 if (binary[k] == '1')
-                 {
-                    this.setPixel((binary.length - k - 1),  i,  true);
-                 }
-                 else
-                 {
-                    this.setPixel((binary.length - k - 1),  i,  false);
-                 }
-              }
-           }
-           
+            int column = 0;
+
+            //convert each letter to ascii then binary and set appropriately
+            //1s are *, 0s are blank
+            for (int i = 0; i < strData.length; i++) // columns
+            {
+                //pull word into character array
+                char[] chars = strData[i].toCharArray();
+                System.out.println(chars);
+
+                for (int j = 0; j < chars.length; j++)
+                {
+                    //converts letter to binary
+                    int temp = Character.getNumericValue(chars[j]);
+                    String temp2 = Integer.toBinaryString(temp);
+                    System.out.println(temp2);
+
+                    //add *s to the rows where binary = 1
+                    char[] binary = temp2.toCharArray();
+                    for (int k = 0; k < binary.length; k++)
+                    {
+                        if (binary[k] == '1')
+                        {
+                            this.setPixel(k,  column,  true);
+                        }
+                        else
+                        {
+                            this.setPixel(k,  column,  false);
+                        }
+                    }
+
+                    column++;
+                }
+
+            }
         }
-     }
-  }
-   //Constructors END
+    }
+    //Constructors END
 
-   //Individual Pixel getter
-   // return is actual value and error flag. Error returns false
-   public boolean getPixel(int row, int col)
-   {
-      if (row <= MAX_WIDTH && col <= MAX_HEIGHT)
-      {
-         return imageData[row][col];
-      }
-      else
-      {
-         return false;
-      }
-   }
+    //Individual Pixel getter
+    // return is actual value and error flag. Error returns false
+    public boolean getPixel(int row, int col)
+    {
+        if (row <= MAX_WIDTH && col <= MAX_HEIGHT)
+        {
+            return imageData[row][col];
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-   //Individual Pixel setter
+    //Individual Pixel setter
     public boolean setPixel(int row, int col, boolean value)
     {
-       if (row <= MAX_WIDTH && col <= MAX_HEIGHT)
-         {
-             imageData[row][col] = value;
-             return true;
-         }
-         else
-         {
+        if (row <= MAX_WIDTH && col <= MAX_HEIGHT)
+        {
+            imageData[row][col] = value;
+            return true;
+        }
+        else
+        {
             return false;
-         }
+        }
     }
 
 
@@ -136,127 +142,129 @@ class BarcodeImage implements Cloneable
     // Checks incoming data for "very conceivable size or null error"
     private boolean checkSize(String[] data)
     {
-       int size = 0;
-       
-       for (int i = 0; i < data.length; i++)
-       {
-           size = size + data[i].length();
-       }
-      
-       if(size > MAX_WIDTH || data == null) //should this be -1?
-       {
-          return false;
-       }
-       else
-       {
-          return true;
-       }
+        int size = 0;
+
+        for (int i = 0; i < data.length; i++)
+        {
+            size = size + data[i].length();
+        }
+
+        if(size > MAX_WIDTH || data == null) //should this be -1?
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     // Optional Testing method
     public void displayToConsole()
     {
-       for(int i = 0; i < MAX_WIDTH; i++) 
-       {
-          for(int j = 0; j < MAX_HEIGHT; j++) 
-          {
-             if (this.imageData[i][j] == true)
-             {
-                System.out.print("*");
-             }
-             else
-             {
-                System.out.print(" ");
-             }
-          }
-       }
+        for(int i = 0; i < MAX_WIDTH; i++)
+        {
+            for(int j = 0; j < MAX_HEIGHT; j++)
+            {
+                if (this.imageData[i][j] == true)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("\n");
+        }
     }
 
     // clone() overwrite cloneable method
     public Object clone()
     {
-       try
-       {
-          return super.clone(); //simple version since type is boolean right?
-       }
-       catch(CloneNotSupportedException e)
-       {
-          return null;
-       }
+        try
+        {
+            return super.clone(); //simple version since type is boolean right?
+        }
+        catch(CloneNotSupportedException e)
+        {
+            return null;
+        }
     }
 }
 
 class DataMatrix implements BarcodeIO
 {
-   public static final char BLACK_CHAR = '*';
-   public static final char WHITE_CHAR = ' ';
+    public static final char BLACK_CHAR = '*';
+    public static final char WHITE_CHAR = ' ';
 
-   private BarcodeImage image;
+    private BarcodeImage image;
 
-   private String text;
+    private String text;
 
-   private int actualWidth, actualHeight;
+    private int actualWidth, actualHeight;
 
-   //defaut constructor
-   DataMatrix()
-   {
-      this.text = "";
-      this.actualHeight = 0;
-      this.actualWidth = 0;
-      this.image = new BarcodeImage();
-   }
-   
-   //barcode constructor
-   DataMatrix(BarcodeImage image)
-   {
-      
-   }
-   
-   //string constructor
-   DataMatrix(String text)
-   {
-      
-   }
+    //defaut constructor
+    DataMatrix()
+    {
+        this.text = "";
+        this.actualHeight = 0;
+        this.actualWidth = 0;
+        this.image = new BarcodeImage();
+    }
 
-   @Override
-   public boolean scan(BarcodeImage bc)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
+    //barcode constructor
+    DataMatrix(BarcodeImage image)
+    {
 
-   @Override
-   public boolean readText(String text)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
+    }
 
-   @Override
-   public boolean generateImageFromText()
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
+    //string constructor
+    DataMatrix(String text)
+    {
 
-   @Override
-   public boolean translateImageToText()
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
+    }
 
-   @Override
-   public void displayTextToConsole()
-   {
-      // TODO Auto-generated method stub
-      
-   }
+    @Override
+    public boolean scan(BarcodeImage bc)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-   @Override
-   public void displayImageToConsole()
-   {
-      // TODO Auto-generated method stub
-      
-   }
+    @Override
+    public boolean readText(String text)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean generateImageFromText()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean translateImageToText()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void displayTextToConsole()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void displayImageToConsole()
+    {
+        // TODO Auto-generated method stub
+
+    }
 }
